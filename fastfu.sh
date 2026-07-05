a87dacc1995bc91bb2c56519c9ae66e88bcf7d36
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-07-05 00:54:21 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -1,6 +1,7 @@
 fastfu(){ awk 'BEGIN{print "#include <stdlib.h>\n#include <stdio.h>\nint main(){int *t,*u,*g,s;s=sizeof(int);g=malloc(s*256);g['x']=1;g['f']=0;while(g['x']){switch(g['f']){"}{
 d["case"]=$1" "$2":";
 d["break"]=$1;
+d["op"]="g["$5"]=g["$2"] "$3" g["$4"]";
 d["realloc"]="t="$1"(g,sizeof(int)*g["$2"]);if(t==NULL){g["$2"]=0;}else{u=g;g=t;free(u);};";
 d["get"]="g["$3"]=g[g["$2"]]";
 d["set"]="g[g["$3"]]=g["$2"]";
