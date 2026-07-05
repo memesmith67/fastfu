123ab2ed4af805d9848ecd6671ea4060100e27b8
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-07-05 00:40:46 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -1,11 +1,9 @@
-#copyright john morris beck july 5 2026 - licened gpl2
-
-fastfu(){ awk 'BEGIN{print "#include <stdlib.h>\nint main(){int *t,*g=malloc(256);while(g['x']){switch(g['f']){"}{
+fastfu(){ awk 'BEGIN{print "#include <stdlib.h>\n#include <stdio.h>\nint main(){int *t,*u,*g,s;s=sizeof(int);g=malloc(s*256);g['x']=1;g['f']=0;while(g['x']){switch(g['f']){"}{
 d["case"]=$1" "$2":";
 d["break"]=$1;
-d["realloc"]="t="$1"(g,sizeof(int)*g["$2"]);t==NULL?g["$2"]=0:(g=t;free(t));";
+d["realloc"]="t="$1"(g,sizeof(int)*g["$2"]);if(t==NULL){g["$2"]=0;}else{u=g;g=t;free(u);};";
 d["get"]="g["$3"]=g[g["$2"]]";
 d["set"]="g[g["$3"]]=g["$2"]";
-d["input"]="t=g[x];while('\n'!=(*(g+t++)=getchar()))";
-d["output"]="t=g[x];while(*(g+t))putchar(*(g+t++));fflush(stdout)"
-o=d[$1];if(!o){exit 1}print o";"}END{print "default:x=0;break;}}free(g);return 0}';};
+d["input"]="t=g["$2"];while('\n'!=(*(g+t++)=getchar()))";
+d["output"]="t=g["$2"];while(*(g+t))putchar(*(g+t++));fflush(stdout)"
+o=d[$1];if(!o){exit 1}print o";"}END{print "default:g['x']=0;break;}}free(g);return 0}';};
