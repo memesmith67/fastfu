fd2602997916e58619f9e1140fc4a0e3335cbfbd
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-07-30 04:47:34 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -1,10 +1,17 @@
-fastfu(){ awk 'BEGIN{print "#include <stdlib.h>\n#include <stdio.h>\nint main(){int *t,*u,*g,s;s=sizeof(int);g=malloc(s*256);g['x']=1;g['f']=0;while(g['x']){switch(g['f']){"}{
-d["case"]=$1" "$2":";
-d["break"]=$1;
-d["op"]="g["$5"]=g["$2"] "$3" g["$4"]";
-d["realloc"]="t="$1"(g,sizeof(int)*g["$2"]);if(t==NULL){g["$2"]=0;}else{u=g;g=t;free(u);};";
-d["get"]="g["$3"]=g[g["$2"]]";
-d["set"]="g[g["$3"]]=g["$2"]";
-d["input"]="t=g["$2"];while('\n'!=(*(g+t++)=getchar()))";
-d["output"]="t=g["$2"];while(*(g+t))putchar(*(g+t++));fflush(stdout)"
-o=d[$1];if(!o){exit 1}print o";"}END{print "default:g['x']=0;break;}}free(g);return 0}';};
+fastfu(){ #copyright john morrris beck 2026 gpl2
+    awk 'function c(x,y){print $2==x?y";":""}
+$1!="fastfu",$1=="fastfu"{
+a=$2;b=$3;c=$4;d=$5;
+c("memcpy",a"("b","c","d")")
+c("malloc",b"="a"("c")")
+c("free",a"("b")")
+c("assign",c"="d)
+c("fgetc",b"="a"("c")")
+c("fputc",a"("b","c")")
+c("fflush",a"("b")")
+c("get",c"=*"d)
+c("set","*"c"="b)
+c("control",b"("c"){")
+c("literal",a)
+c("case",a" "b":")
+c("var",a" "b)}';};
