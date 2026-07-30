a7c83f026c5c8b51edfe25c4e7e7f1bb640cc492
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-07-30 05:53:06 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -1,17 +1,17 @@
 fastfu(){ #copyright john morrris beck 2026 gpl2
-    awk 'function f(x,y){print $2==x?y";":""}
-$1!="fastfu",$1=="fastfu"{
+    awk 'function f(x,y){if($2==x){print y";"}}
+$1!="fastfu"{print} $1=="fastfu"{
 a=$2;b=$3;c=$4;d=$5;
 f("memcpy",a"("b","c","d")")
 f("malloc",b"="a"("c")")
 f("free",a"("b")")
-f("assign",c"="d)
+f("assign",b"="c)
 f("fgetc",b"="a"("c")")
 f("fputc",a"("b","c")")
 f("fflush",a"("b")")
-f("get",c"=*"d)
-f("set","*"c"="b)
+f("get",b"=*"c)
+f("set","*"b"="c)
 f("control",b"("c"){")
-f("literal",a)
+f("literal",b)
 f("case",a" "b":")
-f("var",a" "b)}';};
+f("var",b" "c)}';};
