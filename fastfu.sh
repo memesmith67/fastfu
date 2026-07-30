4087a8ef76a79c2a95af24b38909f73fcb124cad
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-07-30 05:33:55 -0700

    Update fastfu.sh
    f

--- fastfu.sh
+++ fastfu.sh
@@ -1,17 +1,17 @@
 fastfu(){ #copyright john morrris beck 2026 gpl2
-    awk 'function c(x,y){print $2==x?y";":""}
+    awk 'function f(x,y){print $2==x?y";":""}
 $1!="fastfu",$1=="fastfu"{
 a=$2;b=$3;c=$4;d=$5;
-c("memcpy",a"("b","c","d")")
-c("malloc",b"="a"("c")")
-c("free",a"("b")")
-c("assign",c"="d)
-c("fgetc",b"="a"("c")")
-c("fputc",a"("b","c")")
-c("fflush",a"("b")")
-c("get",c"=*"d)
-c("set","*"c"="b)
-c("control",b"("c"){")
-c("literal",a)
-c("case",a" "b":")
-c("var",a" "b)}';};
+f("memcpy",a"("b","c","d")")
+f("malloc",b"="a"("c")")
+f("free",a"("b")")
+f("assign",c"="d)
+f("fgetc",b"="a"("c")")
+f("fputc",a"("b","c")")
+f("fflush",a"("b")")
+f("get",c"=*"d)
+f("set","*"c"="b)
+f("control",b"("c"){")
+f("literal",a)
+f("case",a" "b":")
+f("var",a" "b)}';};
