75cb23b7902a0df013af9a30bf792359723769c1
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-07-30 22:20:06 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -1,5 +1,6 @@
 #copyright john morris beck 2026 gpl2
-fastfu(){ awk 'function f(x,y){if($2==x){print y";"}}
+fastfu(){
+    awk 'function f(x,y){if($2==x){print y";"}}
 $1!="fastfu"{print} $1=="fastfu"{
 a=$2;b=$3;c=$4;d=$5;
 f("memcpy",a"("b","c","d")")
@@ -14,5 +15,8 @@ f("set","*"b"="c)
 f("control",b"("c"){")
 f("literal",b)
 f("case",a" "b":")
-f("var",b" "c)}';};
-irsh(){ awk '{gsub(/[^ ]/,"\\\\&")}1'|awk '{a=$1;b=$2}{system("{ "$0"</tmp/."a">/tmp/."b" 2>/tmp/."b".e;printf %s $?>/tmp/."b".?;}&")}';}
+f("var",b" "c)}';}
+irsh(){ awk '{gsub(/[^ ]/,"\\\\&")}1'|awk '
+{a=$1;b=$2}{c="{ "$0"</tmp/."a">/tmp/."b" 2>/tmp/."
+c=c b".e;printf %s $?>/tmp/."b".?;}&"
+system(c)}';}
