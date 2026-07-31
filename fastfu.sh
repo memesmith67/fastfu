1bfe85d5afe36ffdcd502428b477639a1fb2d81d
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-07-31 00:04:40 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -1,4 +1,4 @@
-#copyright john morris beck 2026 gpl2
+#copyright john morrris beck 2026 gpl2
 fastfu(){ awk 'function f(x,y){if($2==x){print y";"}}
 $1!="fastfu"{print} $1=="fastfu"{
 a=$2;b=$3;c=$4;d=$5;
@@ -19,9 +19,9 @@ irsh(){ awk '{gsub(/[^ ]/,"\\\\&")}1'|awk '
 {a=$1;b=$2}{c="{ "$0"</tmp/."a">/tmp/."b" 2>/tmp/."
 c=c b".e;printf %s $?>/tmp/."b".?;}&";system(c)}';}
 macrosh(){ awk '
-function nc(x){if(ci==length(l)){getline l;return "\n"}
-else{return substr(l,ci,1)}}
-BEGIN{i=256;for(j=9999;--j>i;){r[i]=nc()}
+function nc(x){if(ci==length(l)){if(getline l){
+ci=0;return "\n"}else{return ""}}else{return substr(l,++ci,1)}}
+BEGIN{for(i=255;i++<9999;){r[i]=nc()}i=256;
 while(1){a=r[i++];b=r[i++];c=r[i++];
 if(a=="-"){r[b]-=r[c]}
 else if(a=="<"){r[b]=r[b]<r[c]}
