84ff666b5afb6f4b73bcc81f5b2edb08850030bb
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-01 14:48:48 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -11,19 +11,18 @@ f("control",b"("c"){")
 f("literal",b)
 f("case",a" "b":")
 f("var",b" "c)}';}
-irsh(){ awk '{gsub(/[^ ]/,"\\\\&")}1'|awk '
-{a=$1;b=$2}{c="{ "$0"</tmp/."a">/tmp/."b" 2>/tmp/.";
-c=c b".e;printf %s $?>/tmp/."b".?;}&";system(c)}';}
-macrosh(){ awk 'function nc(x){if(ci==length(l)){
-if(getline l){ci=0;return "\n"}else{return ""}}
-else{return substr(l,++ci,1)}}BEGIN{
-for(i=255;i++<9999;){r[i]=nc()}i=256;
+macrosh(){ awk 'function n(x){if(k==length(l)){
+if(getline l){k=0;return "\n"}else{return ""}}
+else{return substr(l,++k,1)}}
+function f(x,y){if(a==x){r[b]=y}}
+BEGIN{for(i=255;i++<999;){r[i]=n()}i=256;
 while(1){a=r[i++];b=r[i++];c=r[i++];
-if(a=="-"){r[b]-=r[c]}
-else if(a=="<"){r[b]=r[b]<r[c]}
-else if(a=="r"){r[b]=r[r[c]]}
-else if(a=="w"){r[r[c]]=r[b]}
-else if(a=="i"){r[b]=nc()}
-else if(a=="o"){printf "%s",r[b]}
-else if(a=="c"){r[b]=sprintf("%c",r[c])}
-else {exit(0)}}'};
+f("b",c)
+f("-",r[b]-r[c])
+f("<",r[b]<r[c])
+f("r",r[r[c]])
+f("w",r[r[c]]=r[b])
+f("i",n())
+f("o",printf "%s",r[c])
+f("c",sprintf("%c",r[c]))
+f("e",exit(r[c]))}'|sh;};
