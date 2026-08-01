49b0e5f181a064fc1d7b2783901f35dc2f880379
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-01 16:08:34 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -11,18 +11,4 @@ f("control",b"("c"){")
 f("literal",b)
 f("case",a" "b":")
 f("var",b" "c)}';}
-macrosh(){ awk 'function n(x){if(k==length(l)){
-if(getline l){k=0;return "\n"}else{return ""}}
-else{return substr(l,++k,1)}}
-function f(x,y){if(a==x){r[b]=y}}
-BEGIN{for(i=255;i++<999;){r[i]=n()}i=256;
-while(1){a=r[i++];b=r[i++];c=r[i++];
-f("b",c)
-f("-",r[b]-r[c])
-f("<",r[b]<r[c])
-f("r",r[r[c]])
-f("w",r[r[c]]=r[b])
-f("i",n())
-f("o",printf "%s",r[c])
-f("c",sprintf("%c",r[c]))
-f("e",exit(r[c]))}}}'|sh;}
+macrosh(){ sh|sh;}
