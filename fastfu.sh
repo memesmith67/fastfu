45a5a6342f4d09bbe6fa401266b7bea5655314cb
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-01 22:58:08 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -1,14 +1,13 @@
-#copyright john morrris beck 2026 gpl2
-fastfu(){ awk 'function f(x,y){if($2==x){print y";"}}
-$1!="fastfu"{print} $1=="fastfu"{
-a=$2;b=$3;c=$4;d=$5;
-f("malloc",b"="a"("c")")
-f("free",a"("b")")
-f("assign",b"="c)
-f("get",b"=*"c)
-f("set","*"b"="c)
-f("control",b"("c"){")
-f("literal",b)
-f("case",a" "b":")
-f("var",b" "c)}';}
-macrosh(){ sh|sh;}
+#copyright john morris beck 2026 gpl2
+macrosh(){ sh|sh;};
+fastfu(){ awk '$1!="fastfu";$1=="fastfu"{
+d["malloc"]=$3"="$2"("$4")";
+d["free"]=$2"("$3")";
+d["control"]=$3"("$4"){";
+d["literal"]=$3;
+d["case"]=$2" "$3":";
+d["var"]=$3" "$4;
+d["assign"]=$3"="$4;
+d["get"]=$3"=*"$4;
+d["set"]="*"$3"="$4;
+print d[$2]";"}';};
