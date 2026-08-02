d2fa3227ed63c3f441801cf726b58bca3a574712
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-02 00:23:03 -0700

    Update fastfu.sh

--- fastfu.sh
+++ fastfu.sh
@@ -2,7 +2,7 @@
 macrosh(){ sh|sh;}
 fastfu(){ awk '
 "fastfu"!=$1;"fastfu"==$1{
-if ($3$4 !~ /^[a-zA-Z0-9_*()+|-&!~\/]*$/) exit 1
+if($3$4!~/^[a-zA-Z0-9_*():\}+|-&!~\/]*$/)exit 1
 f["malloc"]=$3"="$2"("$4")"
 f["free"]=$2"("$3")"
 f["control"]=$3"("$4"){"
